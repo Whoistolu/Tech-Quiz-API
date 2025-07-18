2b2cc90ae3e92b36993817d391ee5751e9d8094c
Whoistolu <digittolu25@gmail.com>
2025-07-18 14:39:24 +0100

    Implement the Create action and add the option params in ordet to add an Option to a Question

--- app/controllers/api/v1/options_controller.rb
+++ app/controllers/api/v1/options_controller.rb
@@ -3,4 +3,20 @@ class Api::V1::OptionsController < ApplicationController
         @question = Question.find(params[:question_id])
         render json: @question.options
     end
+
+    def create
+        @question = Question.find(params[:question_id])
+        @option = @question.options.build(option_params)
+        if @option.save
+            render json: @option, status: :created
+        else
+            render json: { errors: @option.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def option_params
+        params.require(:option).permit(:content, :correct)
+    end
 end
