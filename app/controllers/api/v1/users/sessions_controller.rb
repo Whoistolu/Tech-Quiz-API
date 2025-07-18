693b16dabc06bca23eb37322a6c6b81015c9bbc8
Whoistolu <digittolu25@gmail.com>
2025-07-18 14:23:24 +0100

    implement the Create and Destry action for the Sessions controller to enable the User to Sign-n and log-out

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -1,27 +1,15 @@
 # frozen_string_literal: true
 
 class Api::V1::Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  def create
+    super do |resource|
+      token = request.env["warden-jwt_auth.token"]
+      render json: { message: "Logged in successfully.", user: resource, token: token }, status: :ok and return
+    end
+  end
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def destroy
+    super
+    render json: { message: "Logged out successfully."}, status: :ok
+  end
 end
