4c3e041868d57fe02f1d40bf3fbf4f12462c91bb
Whoistolu <digittolu25@gmail.com>
2025-07-18 14:29:14 +0100

    Implement the show action for the Quiz controller to show a specific Quiz and its Questions

--- app/controllers/api/v1/quizzes_controller.rb
+++ app/controllers/api/v1/quizzes_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::QuizzesController < ApplicationController
         @quizzes = Quiz.all
         render json: @quizzes
     end
+
+    def show
+        @quiz = Quiz.find(params[:id])
+        render json: @quiz, include :questions
+    end
 end
