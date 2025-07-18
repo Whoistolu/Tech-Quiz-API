1b3f319d734958e0a3baf36ab454dd24a945644b
Whoistolu <digittolu25@gmail.com>
2025-07-18 14:38:06 +0100

    Implement the Index action for the Options Controller to list all Options for a Question

--- app/controllers/api/v1/options_controller.rb
+++ app/controllers/api/v1/options_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::OptionsController < ApplicationController
+    def index
+        @question = Question.find(params[:question_id])
+        render json: @question.options
+    end
 end
