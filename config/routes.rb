02f4403e4e899792645ea10bb9726793d980b180
Whoistolu <digittolu25@gmail.com>
2025-07-19 11:34:47 +0100

    Add the necessary routes and fix the lintet issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,26 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  namespace :api do
+    namespace :v1 do
+      resources :quizzes do
+        resources :questions do
+          resources :options
+        end
+      end
+
+      resources :submissions
+
+      devise_for :users, controllers: {
+        sessions: "api/v1/users/sessions",
+        registrations: "api/v1/users/registrations",
+        passwords: "api/v1/users/passwords",
+        confirmations: "api/v1/users/confirmations",
+        unlocks: "api/v1/users/unlocks",
+        omniauth_callbacks: "api/v1/users/omniauth_callbacks"
+      }
+    end
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
