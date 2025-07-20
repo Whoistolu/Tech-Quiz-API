8d4cfe50c13c1078c67256b9494263feca64f023
Whoistolu <digittolu25@gmail.com>
2025-07-20 11:35:10 +0100

    Add the jti column, Generate a UUID for each existing user, Make the column non-nullable

--- db/migrate/20250720091707_add_jti_to_users.rb
+++ db/migrate/20250720091707_add_jti_to_users.rb
@@ -1,6 +1,13 @@
 class AddJtiToUsers < ActiveRecord::Migration[8.0]
-  def change
+  def up
     add_column :users, :jti, :string
+    User.reset_column_information
+    User.all.each { |user| user.update_column(:jti, SecureRandom.uuid) }
+    change_column_null :users, :jti, false
     add_index :users, :jti, unique: true
+  end
+
+  def down
+    remove_column :users, :jti
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -78,7 +78,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_20_091707) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "jti"
+    t.string "jti", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
