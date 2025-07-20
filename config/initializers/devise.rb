25caf471686d58328a314f4a8e17550e746f8c13
Whoistolu <digittolu25@gmail.com>
2025-07-20 10:23:45 +0100

    Update devise configuration to use /api/v1/users/sign_in for sign in requests

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -29,7 +29,7 @@ Devise.setup do |config|
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.devise[:jwt_secret_key]
     jwt.dispatch_requests = [
-      [ "POST", %r{^/login$} ]
+      [ "POST", %r{^/api/v1/users/sign_in$} ]
     ]
     jwt.revocation_requests = [
       [ "DELETE", %r{^/logout$} ]
